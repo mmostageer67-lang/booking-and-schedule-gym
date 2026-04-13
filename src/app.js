8851fdca8a5755cd7231b954f484a4ef239ae68e
mahmoud <mmostageer67@gmail.com>
2026-04-13 06:06:38 +0200

    Remove API router middleware
    Remove router middleware from app.js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,9 @@
 const express = require('express')
 const morgan = require('morgan')
-const router = require('./routes')
 
 const app = express()
 
 app.use(express.json())
 app.use(morgan('dev'))
-app.use('/api', router)
 
 module.exports = app
