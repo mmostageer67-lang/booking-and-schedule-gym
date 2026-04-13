ac1d0ee64ef949a6df90d60d8af404b7698a9164
mahmoud mostageer <mmostageer67@gmail.com>
2026-04-13 17:33:06 +0200

    genrate tokens

--- src/server.js
+++ src/server.js
@@ -1,11 +1,12 @@
 
 require('dotenv').config()
 const connectDB=require('./config/db')
-const app=require('./app')
+const app=require('./app')  
+  const PORT=process.env.PORT||3000
+
 const startServer=async () => {
     try {
         await connectDB()
-    const PORT=process.env.PORT||3000
 app.listen(PORT,()=>
 {
 console.log(`Server running on port ${PORT}`)  })
